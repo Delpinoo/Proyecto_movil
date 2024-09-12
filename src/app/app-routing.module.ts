9608285fc418072243204030ab692ac233c5ffc3
samueleliazar <samu.reyes@duocuc.cl>
2024-09-11 21:25:29 -0300

    confirmacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'alumno',
     loadChildren: () => import('./page/alumno/alumno.module').then( m => m.AlumnoPageModule)
+  },  {
+    path: 'verificacion',
+    loadChildren: () => import('./page/verificacion/verificacion.module').then( m => m.VerificacionPageModule)
   },
+
 ];
 
 @NgModule({
--- src/app/page/alumno/alumno.page.ts
+++ src/app/page/alumno/alumno.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alumno',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlumnoPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
+  
+  goToverification() {
+    this.navCtrl.navigateForward('/verificacion')
+  }
+
 
   ngOnInit() {
   }
