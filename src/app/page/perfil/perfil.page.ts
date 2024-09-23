aea15d26abab3854e742a02873d29e6c42eeefef
Delpinoo <139150351+Delpinoo@users.noreply.github.com>
2024-09-23 20:19:04 -0300

    perfil-temrinado

--- src/app/page/perfil/perfil.page.ts
+++ src/app/page/perfil/perfil.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -9,9 +10,28 @@ import { NavController } from '@ionic/angular';
 })
 export class PerfilPage implements OnInit {
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
 
+  async cambiarContrasena() {
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: 'La sesion se cerro correctamente',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.router.navigate(['/home']);
+          },
+          cssClass: 'alert-button-white',
+        },
+      ],
+    });
+
+    alert.cssClass = 'custom-alert';
+    
+    await alert.present();
+  }
 }
