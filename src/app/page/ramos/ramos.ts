7f03e3484c86caa35211d32f1c294026f9aeaf00
samueleliazar <samu.reyes@duocuc.cl>
2024-11-02 01:38:27 -0300

    ramosxcarrera

--- src/app/page/ramos/ramos.ts
+++ src/app/page/ramos/ramos.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-ramos',
@@ -7,14 +8,43 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./ramos.page.scss'],
 })
 export class RamosPage implements OnInit {
+  user: any;
+  ramos: any[] = [];
 
-  constructor(private navCtrl: NavController) { }
+  private ramosPorCarrera: { [key: string]: any[] } = {
+    carrera1: [ 
+      { nombre: 'Matemáticas', asistencia: 76, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Programación', asistencia: 72, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Inglés', asistencia: 68, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Cristianismo', asistencia: 54, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] }
+    ],
+    carrera2: [ 
+      { nombre: 'Física', asistencia: 80, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Química', asistencia: 75, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Matemáticas', asistencia: 70, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Cristianismo', asistencia: 50, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] }
+    ],
+    carrera3: [ 
+      { nombre: 'Contabilidad', asistencia: 78, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Economía', asistencia: 77, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Administración', asistencia: 85, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] },
+      { nombre: 'Cristianismo', asistencia: 60, fechas: ['27-10-2024', '24-10-2024', '27-09-2024'] }
+    ],
+  };
 
-  goTolista_alumno() {
-    this.navCtrl.navigateForward('/lista-alumno')
-  }
+  constructor(private navCtrl: NavController, private userService: UserService) {}
 
   ngOnInit() {
+    this.user = this.userService.getCurrentUser();
+    
+    if (this.user) {
+      this.setRamosPorCarrera(this.user.carrera);
+    } else {
+      console.error('No hay usuario logueado.');
+    }
   }
 
+  setRamosPorCarrera(carrera: string) {
+    this.ramos = this.ramosPorCarrera[carrera] || [];
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,16 +5,15 @@ import { User } from './shared/models/user.model';
   providedIn: 'root',
 })
 export class UserService {
-  private usersKey = 'users'
-
+  private usersKey = 'users';
+  private currentUserKey = 'currentUser';
 
   constructor() {}
 
   addUser(user: User): boolean {
     const users: User[] = this.getUsers();
-    
-
     const existingUser = users.find(u => u.correo === user.correo);
+    
     if (existingUser) {
       console.log('El correo ya está registrado');
       return false; 
@@ -22,6 +21,7 @@ export class UserService {
 
     users.push(user); 
     localStorage.setItem(this.usersKey, JSON.stringify(users)); 
+    this.setCurrentUser(user); 
     console.log('Usuarios registrados:', users);
     return true; 
   }
@@ -34,16 +34,33 @@ export class UserService {
   getUserCount(): number {
     return this.getUsers().length; 
   }
-  
+
   login(correo: string, contrasena: string): boolean {
     const users: User[] = this.getUsers();
     const user = users.find(u => u.correo === correo && u.contrasena === contrasena);
-    return user !== undefined; // Retorna true si se encuentra el usuario
+    
+    if (user) {
+      this.setCurrentUser(user); 
+      return true; 
+    }
+    return false; 
   }
 
   logout() {
-    localStorage.removeItem(this.usersKey);
+    this.clearCurrentUser(); 
     console.log('Sesión cerrada');
   }
 
+  setCurrentUser(user: User): void {
+    localStorage.setItem(this.currentUserKey, JSON.stringify(user)); 
+  }
+
+  getCurrentUser(): User | null {
+    const user = localStorage.getItem(this.currentUserKey);
+    return user ? JSON.parse(user) : null; 
+  }
+
+  clearCurrentUser(): void {
+    localStorage.removeItem(this.currentUserKey); 
+  }
 }
